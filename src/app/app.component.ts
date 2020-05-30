6997a698b0c5635b2ac121957d3f6055f218d95e
Dominika Zając <dominikaz@qualtrics.com>
2020-05-30 23:47:24 +0200

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'button-example';
+
+  grajDzwiek(sciezka) {
+    let audio = new Audio();
+    audio.src = sciezka;
+    audio.load();
+    audio.play();
+  }
+
+
+  dzwiekTrabki(){
+    this.grajDzwiek("https://raw.githubusercontent.com/dominika-zajac/button-example/master/src/assets/trumpets.wav");
+  }
+
+  dzwiekGitary(){
+    this.grajDzwiek("https://raw.githubusercontent.com/dominika-zajac/button-example/master/src/assets/guitar.wav");
+  }
+
+  dzwiekPianina(){
+    this.grajDzwiek("https://raw.githubusercontent.com/dominika-zajac/button-example/master/src/assets/piano.wav");
+  }
+
+  dzwiekSkrzypiec(){
+    this.grajDzwiek("https://raw.githubusercontent.com/dominika-zajac/button-example/master/src/assets/violin.mp3");
+  }
+
+  dzwiekFletu(){
+    this.grajDzwiek("https://raw.githubusercontent.com/dominika-zajac/button-example/master/src/assets/flute.wav");
+  }
 }
